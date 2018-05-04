a885afa8460b3178e2d7032929820e2f53d01634
William <William@MacBook-Pro-2.local>
2018-05-04 14:05:16 -0700

    added css

--- assets/javascript/nyt.js
+++ assets/javascript/nyt.js
@@ -2,24 +2,20 @@ $(document).ready(function () {
     
     var queryString="obama";
 
-
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch&limit=5.json";
+    
+    
+    
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
       'api-key': "0a6c9e486b8c4e0baefa99974e943acd",
-      'q': queryString,
-      'begin_date': $("#start").val()+"0101",
-      'end_date': $("#end").val()+"1231"
+      'q': queryString
     });
+
     $.ajax({
       url: url,
       method: 'GET'
     }).done(function(result) {
       console.log(result);
-    //   queryString = $("#search").val();
-
-
-
-
     });
 });
 
